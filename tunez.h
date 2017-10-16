2d9308d28f80b4f1efdc3810fd2f3e3e360ba089
nikitaBor <nborisov@stuy.edu>
2017-10-16 15:24:25 -0400

    Update tunez.h

--- tunez.h
+++ tunez.h
@@ -4,9 +4,23 @@ struct song_node{
   struct song_node *next;
 };
 
+//list
 void print_list(struct song_node *);
-struct song_node * insert_front(struct song_node *);
-struct song_node * insert_alph(struct song_node *);
-struct song_node * find_by_name(struct song_node*);
-struct song_node * find_by_artist(struct song_node*);
-struct song_node * find_random(struct song_node*);
+struct song_node * insert_front(struct song_node *, char[], char[]);
+struct song_node * insert_alph(struct song_node *, char[], char[]);
+struct song_node * search_list_name(struct song_node *, char[]);
+struct song_node * search_list_artist(struct song_node *, char[]);
+struct song_node * search_list_random(struct song_node *);
+void delete_list_song(struct song_node *, char[]);
+void free_list(struct song_node *);
+
+//array
+struct song_node * add_song(struct song_node *[], char[], char[]);
+struct song_node * search_lib_name(struct song_node *[], char[]);
+struct song_node * search_lib_artist(struct song_node *[], char[]);
+void print_lib_letter(struct song_node *[], char);
+void print_lib_artist(struct song_node *[], char[]);
+void print_lib(struct song_node *[]);
+void print_random(struct song_node *[], int);
+void delete_lib_song(struct song_node *[], char[]);
+void free_lib(struct song_node *[]);
