5cabb2faf12b1795a4d97f5fb667a6fcb6a61121
nikitaBor <nborisov@stuy.edu>
2017-10-19 15:25:08 -0400

    adjusted for swap of artist and name

--- tunezLIB.c
+++ tunezLIB.c
@@ -8,32 +8,32 @@
 //inserts song into library under correct letter alphabetically 
 //return pointer to beginning of list with that song
 struct song_node * add_song(struct song_node * lib[], char newName[], char newArtist[]){
-  lib[newName[0]-97] = insert_alph(lib[newName[0]-97], newName, newArtist);
-  return lib[newName[0]-97];
+  lib[newArtist[0]-97] = insert_alph(lib[newArtist[0]-97], newName, newArtist);
+  return lib[newArtist[0]-97];
 }
 
 //search_lib_name
-//looks for node based on song name and artist and returns pointer to it by
-//... going directly to the song list with the correct ltr
-//returns null if song is not found
+//searches for node in library with song name by looking through
+//... all lists in order
+//returns null if song not found
 struct song_node * search_lib_name(struct song_node * lib[],
 				   char findName[], char findArtist[]){
-  return search_list_name(lib[findName[0]-97],findName,findArtist);
-}
-
-//search_lib_artist
-//searches for first node in library with artist name by looking through
-//... all lists in order
-//returns null if artist not found
-struct song_node * search_lib_artist(struct song_node * lib[],
-				     char findArtist[]){
   char c = 97;
   struct song_node *n = 0;
   while(c<123){ //goes up to 'z' list
-    if(n = search_list_artist(lib[c-97], findArtist)){return n;}
+    if(n = search_list_name(lib[c-97], findName, findArtist)){return n;}
     c++;
   }
     return n; //n would still be null if here
+}
+
+//search_lib_artist
+//looks for first node with artist name by
+//... going directly to the song list with same letter as artist
+//returns null if song is not found
+struct song_node * search_lib_artist(struct song_node * lib[],
+				     char findArtist[]){
+  return search_list_artist(lib[findArtist[0]-97], findArtist);
 }
 
 //print_lib_letter
@@ -49,11 +49,7 @@ void print_lib_letter(struct song_node * lib[], char ltr){
 //print_lib_artist
 //prints all songs by an findArtist
 void print_lib_artist(struct song_node *lib[], char findArtist[]){
-  char c = 97;
-  while(c<123){ //goes up to 'z' list
-    print_list_artist(lib[c-97],findArtist);//print songs by artist in each list
-    c++;
-  }
+  print_list_artist(lib[findArtist[0]-97],findArtist);//print songs by artist in artist's list
 }
 
 //print_lib
@@ -81,7 +77,7 @@ void print_lib_random(struct song_node * lib[], int numSongs){
 //deletes findName by findArtist by going to list with correct letter of findName
 //does nothing if song isn't present
 void delete_lib_song(struct song_node * lib[], char findName[], char findArtist[]){
-    lib[findName[0]-97] = delete_list_song(lib[findName[0]-97], findName, findArtist);
+    lib[findArtist[0]-97] = delete_list_song(lib[findArtist[0]-97], findName, findArtist);
 }
 
 //free_lib
